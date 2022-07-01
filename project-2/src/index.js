c45e2abe0dc747211211011d9bfdf0008a5761a8
qo <submatrices@gmail.com>
2022-07-01 12:46:26 +0300

    Project 2, Lesson 16. - Add Header, Footer and MainContent Components to page "Why I'm excited to be learning React".

--- project-2/src/index.js
+++ project-2/src/index.js
@@ -2,25 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src={require("./react-logo.png")} alt="react-logo" width="100px" />
+            </nav>
+        </header>
+    );
+}
+
+function MainContent() {
+    return (
+        <div>
+            <h1>Why I'm excited to be learning React</h1>
+            <ol>
+                <li>React is highly maintained library and used by a lot of companies</li>
+                <li>I can instantly see the result via my browser</li>
+                <li>There is a huge library of React documentation</li>
+            </ol>
+        </div>
+    );
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>2022, qo</small>
+        </footer>
+    );
+}
+
 function Page() {
     return (
         <div>
-            <header>
-                <nav>
-                    <img src={require("./react-logo.png")} alt="react-logo" width="100px" />
-                </nav>
-            </header>
-            <div>
-                <h1>Why I'm excited to be learning React</h1>
-                <ol>
-                    <li>React is highly maintained library and used by a lot of companies</li>
-                    <li>I can instantly see the result via my browser</li>
-                    <li>There is a huge library of React documentation</li>
-                </ol>
-            </div>
-            <footer>
-                2022, qo
-            </footer>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     )
 }

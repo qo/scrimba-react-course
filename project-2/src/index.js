05acd05f50603c72fd15f858c641f9f3b52e6085
qo <submatrices@gmail.com>
2022-07-01 12:30:35 +0300

    Project 2, Lesson 14. - Add header and footer to page "Why I'm excited to be learning React".

--- project-2/src/index.js
+++ project-2/src/index.js
@@ -5,12 +5,22 @@ import './index.css';
 function Page() {
     return (
         <div>
-            <h1>Why I'm excited to be learning React</h1>
-            <ol>
-                <li>React is highly maintained library and used by a lot of companies</li>
-                <li>I can instantly see the result via my browser</li>
-                <li>There is a huge library of React documentation</li>
-            </ol>
+            <header>
+                <nav>
+                    <img src={require("./react-logo.png")} alt="react-logo" width="100px" />
+                </nav>
+            </header>
+            <div>
+                <h1>Why I'm excited to be learning React</h1>
+                <ol>
+                    <li>React is highly maintained library and used by a lot of companies</li>
+                    <li>I can instantly see the result via my browser</li>
+                    <li>There is a huge library of React documentation</li>
+                </ol>
+            </div>
+            <footer>
+                2022, qo
+            </footer>
         </div>
     )
 }

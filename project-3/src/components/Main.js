1ca6d6cb86cb4778f04173fe7374a1c02e7139ac
qo <submatrices@gmail.com>
2022-07-02 13:24:54 +0300

    Project 3, Lesson 25. - Add Main styling via class names to "ReactFacts".

--- project-3/src/components/Main.js
+++ project-3/src/components/Main.js
@@ -3,7 +3,14 @@ import "./Main.css"
 export default function Main() {
     return (
         <div className="main">
-            Main goes here
+            <div className="title">Fun facts about React</div>
+            <ul className="facts-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </div>
     );
 }

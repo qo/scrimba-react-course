47f034f3c0c270f9bca8dcd4b0faae3fe27cce99
qo <submatrices@gmail.com>
2022-07-04 17:19:39 +0300

    Project 5, Lesson 2.15. - Add props to Card and Cards components instead of hard-coding its properties.

--- project-5/src/components/Cards/Card/Card.js
+++ project-5/src/components/Cards/Card/Card.js
@@ -1,26 +1,26 @@
 import "./Card.css"
-import photo from "./images/photo.png"
 import star from "./images/star.svg"
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props);
     return(
         <div className="card">
-            <img className="photo" src={photo} alt=""/>
+            <img className="image" src={props.data.image} alt=""/>
             <div className="first-row">
                 <img className="star" src={star} alt=""/>
-                <div className="rating">5.0</div>
-                <div className="amount-of-ratings">(6)</div>
+                <div className="rating">{props.data.rating}</div>
+                <div className="ratings-count">({props.data.ratingsCount})</div>
                 <div className="dot">·</div>
-                <div className="location">USA</div>
+                <div className="location">{props.data.location}</div>
             </div>
             <div className="second-row">
                 <div className="title">
-                    Life lessons with Katie Zaferes
+                    {props.data.title}
                 </div>
             </div>
             <div className="third-row">
                 <div className="price">
-                    From $136 /
+                    From ${props.data.price} /
                 </div>
                 <div className="person-word">
                      person
--- project-5/src/components/Cards/Cards.js
+++ project-5/src/components/Cards/Cards.js
@@ -1,10 +1,13 @@
 import "./Cards.css"
 import Card from "./Card/Card";
 
-export default function Cards() {
+export default function Cards(props) {
+    console.log(props);
     return (
         <div className="cards">
-            <Card />
+            <Card data={props.data[0]} />
+            <Card data={props.data[1]} />
+            <Card data={props.data[2]} />
         </div>
     )
 }

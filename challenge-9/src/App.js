a063b286cbef7ef961a6540483941577d48a5e7b
qo <submatrices@gmail.com>
2022-07-15 13:16:12 +0300

    Challenge 9, Lesson 3.19 (useState & Ternary Practice). - Solve challenge using useState.

--- challenge-9/src/App.js
+++ challenge-9/src/App.js
@@ -1,14 +1,18 @@
-import React from "react"
+import React, {useState} from "react"
 import "./style.css"
 
 export default function App() {
 
-  const isGoingOut = true;
+  const [isGoingOut, setIsGoingOut] = useState(true);
+
+  function handleToggleIsGoingOut() {
+      setIsGoingOut(prevState => !prevState);
+  }
 
   return (
       <div className="state">
         <h1 className="state--title">Do I feel like going out tonight?</h1>
-        <div className="state--value">
+        <div className="state--value" onClick={handleToggleIsGoingOut}>
           <h1>{isGoingOut ? "Yes" : "No"}</h1>
         </div>
       </div>

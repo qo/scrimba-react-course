c613205ebe64214e55f364d16bf6f6968d529eee
qo <submatrices@gmail.com>
2022-07-15 12:26:56 +0300

    Challenge 8, Lesson 3.15 (useState "Counter" Challenge). - Change the names of functions (handleIncrement() => add(), handleDecrement => subtract()). - Use callback functions for setCount.

--- challenge-8/src/App.js
+++ challenge-8/src/App.js
@@ -9,21 +9,21 @@ export default function App() {
 
   const [count, setCount] = useState(0);
 
-  function handleIncrement() {
-    setCount(count + 1);
+  function add() {
+    setCount(prevCount => prevCount + 1);
   }
 
-  function handleDecrement() {
-    setCount(count - 1);
+  function subtract() {
+    setCount(prevCount => prevCount - 1);
   }
 
   return (
       <div className="counter">
-        <button className="counter--minus" onClick={handleDecrement}>–</button>
+        <button className="counter--minus" onClick={subtract}>–</button>
         <div className="counter--count">
           <h1>{count}</h1>
         </div>
-        <button className="counter--plus" onClick={handleIncrement}>+</button>
+        <button className="counter--plus" onClick={add}>+</button>
       </div>
   )
 }

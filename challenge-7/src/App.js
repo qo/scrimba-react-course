4e2365c1230e5c6aab5c7fb4052d35765afc1d6d
qo <submatrices@gmail.com>
2022-07-15 12:14:43 +0300

    Challenge 7, Lesson 3.13 (useState Destructuring). - Solve the challenge via useState Hook.

--- challenge-7/src/App.js
+++ challenge-7/src/App.js
@@ -1,18 +1,31 @@
-import React, { useState } from "react"
 import "./style.css"
+import React from "react"
 
 export default function App() {
-  /**
-   * Challenge: Replace our hard-coded "Yes" on the page with
-   * some state initiated with React.useState()
-   */
-  const [result, setResult] = useState("Yes")
-  return (
-      <div className="state">
-        <h1 className="state--title">Is state important to know?</h1>
-        <div className="state--value">
-          <h1>{result}</h1>
+
+    const [isImportant, setIsImportant] = React.useState("Yes");
+
+    /**
+     * Challenge:
+     * 1. Create a function called `handleClick` that runs
+     *    setIsImportant("No")
+     * 2. add a click event listener to the div.state--value
+     *    that runs `handleClick` when the div is clicked.
+     */
+
+    function handleClick() {
+        if (isImportant === "Yes")
+            setIsImportant("No");
+        else
+            setIsImportant("Yes");
+    }
+
+    return (
+        <div className="state">
+            <h1 className="state--title">Is state important to know?</h1>
+            <div className="state--value" onClick={handleClick}>
+                <h1>{isImportant}</h1>
+            </div>
         </div>
-      </div>
-  )
+    )
 }

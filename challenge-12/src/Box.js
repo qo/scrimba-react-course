f0ec336d7e79ad30ac702cf9d3b1c6579a411117
qo <submatrices@gmail.com>
2022-07-17 14:58:53 +0300

    Challenge 12, Lesson 3.30 (Boxes Challenge Pt. 3.1 - local state). - Create state controlling whether this box is "on" or "off". Use the incoming "props.on" to determine the initial state (this is not considered a good practice!). - Create an event listener so when the box is clicked, it toggles from "on" to "off".

--- challenge-12/src/Box.js
+++ challenge-12/src/Box.js
@@ -1,19 +1,26 @@
 import "./Box.css"
+import {useState} from "react";
 
 export default function Box(props) {
+
+    const [isOn, setIsOn] = useState(props.on);
+
+    function toggleBox() {
+        setIsOn(isOn => !isOn);
+    }
 
     // If box is off, set background color to "none",
     // otherwise set it to dark blue if dark theme is on
     // or to light blue if dark theme is off.
     let boxBackgroundColor = {
         backgroundColor:
-            (props.on ?
+            (isOn ?
                 (props.darkTheme ? "#1e447d" : "#5d8dd4") :
-                "none")
+                "transparent")
     };
 
     return (
-        <div className="box" style={boxBackgroundColor}>
+        <div className="box" style={boxBackgroundColor} onClick={toggleBox}>
             <p>
                 {props.id}
             </p>

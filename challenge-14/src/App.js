0cbb90207155da2e29b7bcf6014e6ff096bfaa98
qo <submatrices@gmail.com>
2022-07-18 01:10:05 +0300

    Challenge 14, Lesson 3.37 (Conditional rendering practice). - If there are no unread messages, display "You're all caught up!". - If there's exactly 1 unread message, display "You have 1 unread message". - If there are n > 1 unread messages, display "You have <n> unread messages".

--- challenge-14/src/App.js
+++ challenge-14/src/App.js
@@ -4,14 +4,41 @@ import React, {useState} from "react";
 export default function App() {
   const [messages, setMessages] = useState(["a", "b"]);
 
-  /**
-   * Challenge:
-   * - Only display the <h1> below if there are unread messages
-   */
+    /**
+     * Challenge:
+     * - If there are no unread messages, display "You're all caught up!"
+     * - If there are > 0 unread messages, display "You have <n> unread
+     *   message(s)"
+     *      - If there's exactly 1 unread message, it should read "message"
+     *        (singular)
+     */
+
+    function addMessage() {
+        setMessages(messages => [...messages, "dummy"]);
+    }
+
+    function subMessage() {
+        if (messages.length > 1)
+            setMessages(messages => messages.slice(messages.length - 1));
+        else
+            setMessages([]);
+    }
 
   return (
-      <div>
-        { messages.length && <h1>You have { messages.length } unread messages!</h1> }
+      <div className="App">
+          <div className="msg-amt">
+              {
+                  messages.length
+                      ?
+                      <h1>You have {messages.length} unread { messages.length > 1 ? "messages" : "message"}</h1>
+                      :
+                      <h1>You're all caught up!</h1>
+              }
+          </div>
+          <div className="change-amt-of-msg">
+              <button onClick={addMessage}>+</button>
+              <button onClick={subMessage}>-</button>
+          </div>
       </div>
   )
 }

542b4e70fb5b20fe7b63e156c2a0e38b64f45eea
qo <submatrices@gmail.com>
2022-07-19 18:42:50 +0300

    Challenge 18, Lesson 3.62 (useEffect cleanup function). - Fix Memory Leak via adding cleanup function (in which remove the "resize" event listener) to useEffect.

--- challenge-18/src/App.js
+++ challenge-18/src/App.js
@@ -3,12 +3,6 @@ import WindowTracker from "./WindowTracker"
 import "./style.css"
 
 export default function App() {
-  /**
-   * Challenge:
-   * 1. Create state called `show`, default to `true`
-   * 2. When the button is clicked, toggle `show`
-   * 3. Only display `<WindowTracker>` if `show` is `true`
-   */
 
   const [show, setShow] = React.useState(true)
 

47f4a9eded7c5ea86f038139405e987da7abe3aa
qo <submatrices@gmail.com>
2022-07-19 22:30:21 +0300

    Challenge 19, Lesson 4.2 (Add Dark/Light modes to ReactFacts site). - Add darkMode state for an App component. - Pass darkMode to Navbar and Main components to let them apply styles depending on the current darkMode state defined in App. - Add toggleDarkMode function that swaps modes via setDarkMode. - Pass toggleDarkMode to Navbar so it can change the current state of darkMode via slider.

---
+++ challenge-19/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react"
+import reactIconSmall from "../images/react-icon-small.svg"
+import "../style.css"
+
+export default function Navbar(props) {
+    return (
+        <nav
+            className={props.darkMode ? "dark": ""}
+        >
+            <img
+                className="nav--logo_icon"
+                src={reactIconSmall}
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+
+            <div
+                className="toggler"
+            >
+                <p className="toggler--light">Light</p>
+                <div
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
+    )
+}

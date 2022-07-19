d6cf3e13ee3874d46e290c82302fbeece0bcfee6
qo <submatrices@gmail.com>
2022-07-19 14:30:34 +0300

    Challenge 17, Lesson 3.59 (useEffect: when to use dependencies). - Combine `count` with the request URL so pressing the "Get Next Character" button will get a new character from the Star Wars API.

--- challenge-17/src/App.js
+++ challenge-17/src/App.js
@@ -2,18 +2,42 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0)
+    const [starWarsData, setStarWarsData] = useState({});
+    const [count, setCount] = useState(1);
 
-  console.log("Component rendered")
+    /**
+     * Challenge: Combine `count` with the request URL
+     * so pressing the "Get Next Character" button will
+     * get a new character from the Star Wars API.
+     * Remember: don't forget to consider the dependencies
+     * array!
+     */
 
-  useEffect(() => {
-      console.log("Effect function ran");
-  }, [count]);
+    alert("Rendering...")
 
-  return (
-      <div>
-        <h2>The count is {count}</h2>
-        <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
-      </div>
-  )
+    useEffect(function() {
+        alert("Getting data from API...");
+
+        fetch(`https://swapi.dev/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, [count]);
+
+    return (
+        <div>
+            { starWarsData &&
+                <div className="character-data">
+                    <h1>{starWarsData.name}</h1>
+                    <h2>Gender: {starWarsData.gender}</h2>
+                    <h2>Height: {starWarsData.height} cm</h2>
+                    <h2>Weight: {starWarsData.mass} kg</h2>
+                    <h2>Hair Color: {starWarsData.hair_color}</h2>
+                    <h2>Skin Color: {starWarsData.skin_color}</h2>
+                    <h2>Eye Color: {starWarsData.eye_color}</h2>
+                    <h2>Birth Year: {starWarsData.birth_year}</h2>
+                </div>
+            }
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+        </div>
+    )
 }

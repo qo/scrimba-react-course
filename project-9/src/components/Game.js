f9f1a320813e0e6a832514d93e5b19ade04406e9
qo <romance@mail.com>
2022-07-24 19:18:17 +0300

    Project 9, Lesson 4.14 (Tenzies: Map array to Die components). - Create dice state instead of numbers state. - Initialize dice state with 10 { number: <random number from 1 to 6>, isActive: false } objects. - Create diceElements array containing Die components.

--- project-9/src/components/Game.js
+++ project-9/src/components/Game.js
@@ -1,37 +1,50 @@
 import "./Game.css"
+import {useState} from "react";
 import Die from "./Die"
 
 /**
  * Challenge:
  *
- * Write a function (getDiceNumbers) that returns an array
- * of 10 random numbers between 1-6 inclusive.
+ * Create state to hold our array of numbers. (Initialize
+ * the state by calling our `allNewDice` function so it
+ * loads all new dice as soon as the app loads)
  *
- **/
+ * Map over the state numbers array to generate our array
+ * of Die elements and render those in place of our
+ * manually-written 10 Die elements.
+ */
 
 export default function Game() {
 
-    function getRandomDiceNumbers() {
-        let numbers = [];
-        for (let i = 0; i < 10; i++)
-            numbers.push(Math.ceil(Math.random() * 6));
-        return numbers;
-    }
-    console.log(getRandomDiceNumbers());
+    const numbers = [];
+    for (let i = 0; i < 10; i++)
+        numbers.push(Math.ceil(Math.random() * 6));
+
+    const [dice, setDice] = useState(
+        () => {
+            return numbers.map(
+                number => {
+                    return {
+                        number: number,
+                        isActive: false,
+                    }
+                }
+            )
+        }
+    );
+
+    let diceElements = dice.map(
+        die =>
+            <Die
+                number={die.number}
+                isActive={die.isActive}
+            />
+    );
 
     return (
         <div className="Game">
             <div className="dice">
-                <Die number={1} isActive={false} />
-                <Die number={2} isActive={true} />
-                <Die number={3} isActive={false} />
-                <Die number={4} isActive={true} />
-                <Die number={5} isActive={false} />
-                <Die number={6} isActive={true} />
-                <Die number={7} isActive={false} />
-                <Die number={8} isActive={true} />
-                <Die number={9} isActive={false} />
-                <Die number={0} isActive={true} />
+                {diceElements}
             </div>
         </div>
     )

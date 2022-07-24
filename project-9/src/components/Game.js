5657ea053a957115e5bbbdf9ed41c0836a840191
qo <romance@mail.com>
2022-07-24 20:10:37 +0300

    Project 9, Lesson 4.15 (Tenzies: Roll Dice Button). - Add roll() function that updates dice state by filling every element of dice array with new random numbers. - Add a "Roll" button that has an onClick event listener calling roll() function. - Add some styling (center dice buttons, center Roll button, add Roll button styles, etc.).

--- project-9/src/components/Game.js
+++ project-9/src/components/Game.js
@@ -3,15 +3,12 @@ import {useState} from "react";
 import Die from "./Die"
 
 /**
- * Challenge:
+ * Challenge: Create a `Roll Dice` button that will re-roll
+ * all 10 dice
  *
- * Create state to hold our array of numbers. (Initialize
- * the state by calling our `allNewDice` function so it
- * loads all new dice as soon as the app loads)
- *
- * Map over the state numbers array to generate our array
- * of Die elements and render those in place of our
- * manually-written 10 Die elements.
+ * Clicking the button should generate a new array of numbers
+ * and set the `dice` state to that new array (thus re-rendering
+ * the array to the page)
  */
 
 export default function Game() {
@@ -41,11 +38,36 @@ export default function Game() {
             />
     );
 
+    function roll() {
+
+        for (let i = 0; i < 10; i++)
+            numbers[i] = Math.ceil(Math.random() * 6);
+
+        setDice(
+            () => {
+                    return numbers.map(
+                        number => {
+                            return {
+                                number: number,
+                                isActive: false,
+                            }
+                        }
+                    )
+                }
+        );
+    }
+
     return (
         <div className="Game">
             <div className="dice">
                 {diceElements}
             </div>
+            <button
+                className="roll-button"
+                onClick={roll}
+            >
+                Roll
+            </button>
         </div>
     )
 }

1b1e84fda4a5d0106bf7d1c0659afddd40cc5758
qo <72982727+qo@users.noreply.github.com>
2022-07-26 18:31:51 +0300

    Project 10, Lesson 4.26 (Quizzical - Solo Project). - Add results array with fields identical to the ones opentdb.com. - Add quizElements array containing all the question-answer elements (<div> with a question and <input> with all the answers).

--- project-10/src/pages/Quiz.js
+++ project-10/src/pages/Quiz.js
@@ -2,7 +2,63 @@ import {useState} from "react";
 
 export default function Quiz() {
 
-    const questions = ["a", "b", "c"];
+    const results = [
+        {
+            question: "Question 1",
+            correct_answer: "A",
+            incorrect_answers: ["B", "C", "D"]
+        },
+        {
+            question: "Question 2",
+            correct_answer: "B",
+            incorrect_answers: ["A", "C", "D"]
+        },
+        {
+            question: "Question 3",
+            correct_answer: "C",
+            incorrect_answers: ["A", "B", "D"]
+        },
+        {
+            question: "Question 4",
+            correct_answer: "D",
+            incorrect_answers: ["A", "B", "C"]
+        },
+    ];
+
+    const quizElements = results.map(
+
+        result => {
+
+            const answers = result.incorrect_answers;
+            answers.push(result.correct_answer);
+
+            const answerElements = answers.map(
+                answer =>
+                    <div className="radio">
+                        <label>
+                            <input
+                                type="radio"
+                                value={answer}
+                            />
+                            {answer}
+                        </label>
+                    </div>
+            )
+
+            return (
+                <div>
+                    <div className="question">
+                        { result.question }
+                    </div>
+                    <div className="answers">
+                        <form>
+                            { answerElements }
+                        </form>
+                    </div>
+                </div>
+            )
+        }
+    )
 
     const [isFinished, setIsFinished] = useState(false);
     const [score, setScore] = useState(0);
@@ -11,7 +67,7 @@ export default function Quiz() {
         ?
         <div>
             <div className="result">
-                You scored {score}/{questions.length} correct answers
+                You scored {score}/{results.length} correct answers
             </div>
             <div
                 className="restart-button"
@@ -33,7 +89,7 @@ export default function Quiz() {
     return (
         <div className="Quiz">
             <div className="main">
-
+                { quizElements }
             </div>
             <div className="bottom">
                 { bottom }

9720262d93e8f706e3bbf24274b14ed4c4813eb1
qo <72982727+qo@users.noreply.github.com>
2022-07-27 12:01:31 +0300

    Project 10, Lesson 4.26 (Quizzical - Solo Project). - Add Intro.css and Quiz.css files. - Add a bunch of styles for both <Intro> and <Quiz> pages from a Figma Project (apply fonts for <Intro> and some elements of <Quiz>, add button styles for <Intro> and some button of <Quiz>, etc.). - Rename "results" list to "tasks". - For answers, use <div>s instead of <radio>s. - Assign an id for every answer (id = <this answer task (question) index> * <maximum number of answers> + <this answer index>). - Add "chosenAnswer" state defined with id of a clicked answer (TODO: make it a list to store all chosen answers instead of a single one). - Apply answer styles depending on whether answer is chosen or not.

--- project-10/src/pages/Intro.js
+++ project-10/src/pages/Intro.js
@@ -1,3 +1,5 @@
+import "./Intro.css"
+
 export default function Intro(props) {
     return (
         <div className="Intro">
--- project-10/src/pages/Quiz.js
+++ project-10/src/pages/Quiz.js
@@ -1,8 +1,9 @@
+import "./Quiz.css"
 import {useState} from "react";
 
 export default function Quiz() {
 
-    const results = [
+    const tasks = [
         {
             question: "Question 1",
             correct_answer: "A",
@@ -11,7 +12,7 @@ export default function Quiz() {
         {
             question: "Question 2",
             correct_answer: "B",
-            incorrect_answers: ["A", "C", "D"]
+            incorrect_answers: ["A"]
         },
         {
             question: "Question 3",
@@ -25,49 +26,54 @@ export default function Quiz() {
         },
     ];
 
-    const quizElements = results.map(
+    const [isFinished, setIsFinished] = useState(false);
+    const [score, setScore] = useState(0);
+    const [chosenAnswerID, setChosenAnswerID] = useState(-1);
 
-        result => {
+    // Each task contains 1 question, 1 correct answer and 1/3 incorrect answers.
+    const quizElements = tasks.map(
+        (task, taskIndex) => {
+            const answerContents = task.incorrect_answers;
+            answerContents.push(task.correct_answer);
 
-            const answers = result.incorrect_answers;
-            answers.push(result.correct_answer);
+            const answers = answerContents.map(
+                (answerContent, answerIndex)  => {
+                    return {
+                        // 4 is the maximum amount of answers.
+                        id: taskIndex * 4 + answerIndex,
+                        content: answerContent
+                    }
+                }
+            )
 
             const answerElements = answers.map(
                 answer =>
-                    <div className="radio">
-                        <label>
-                            <input
-                                type="radio"
-                                value={answer}
-                            />
-                            {answer}
-                        </label>
+                    <div
+                        className={`answer + ${chosenAnswerID === answer.id && "chosen"}`}
+                        onClick={() => setChosenAnswerID(answer.id)}
+                    >
+                        { answer.content }
                     </div>
             )
 
             return (
                 <div>
                     <div className="question">
-                        { result.question }
+                        { task.question }
                     </div>
                     <div className="answers">
-                        <form>
-                            { answerElements }
-                        </form>
+                        { answerElements }
                     </div>
                 </div>
             )
         }
     )
 
-    const [isFinished, setIsFinished] = useState(false);
-    const [score, setScore] = useState(0);
-
     const bottom = isFinished
         ?
         <div>
             <div className="result">
-                You scored {score}/{results.length} correct answers
+                You scored {score}/{tasks.length} correct answers
             </div>
             <div
                 className="restart-button"
